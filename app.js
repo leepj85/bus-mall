06e14d377c3f49c928274f26fb13e32159c35b30
Peter Lee <leepj85@gmail.com>
2019-06-19 21:50:10 -0700

    uncommented Product Objects and reset maxClicks to 25

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ var rightProductImageEl = document.getElementById('right-product-image');
 // var resultsContainer = document.getElementById('results');
 
 var clickCount =  0;
-var maxClicks = 5;
+var maxClicks = 25;
 
 // CONSTRUCTOR
 var Product = function(imgSource = 'default.jpg', name, timesClicked, timesShown) {
@@ -167,20 +167,20 @@ var buildProducts = function() {
   new Product('./img/boots.jpg', 'boots');
   new Product('./img/breakfast.jpg', 'breakfast');
   new Product('./img/bubblegum.jpg', 'bubblegum');
-  // new Product('./img/chair.jpg', 'chair');
-  // new Product('./img/cthulhu.jpg', 'cthulhu');
-  // new Product('./img/dog-duck.jpg', 'dog-duck');
-  // new Product('./img/dragon.jpg', 'dragon');
-  // new Product('./img/pen.jpg', 'pen');
-  // new Product('./img/pet-sweep.jpg', 'pet-sweep');
-  // new Product('./img/scissors.jpg', 'scissors');
-  // new Product('./img/shark.jpg', 'shark');
-  // new Product('./img/sweep.png', 'sweep');
-  // new Product('./img/tauntaun.jpg', 'tauntaun');
-  // new Product('./img/unicorn.jpg', 'unicorn');
-  // new Product('./img/usb.gif', 'usb');
-  // new Product('./img/water-can.jpg', 'water-can');
-  // new Product('./img/wine-glass.jpg', 'wine-glass');
+  new Product('./img/chair.jpg', 'chair');
+  new Product('./img/cthulhu.jpg', 'cthulhu');
+  new Product('./img/dog-duck.jpg', 'dog-duck');
+  new Product('./img/dragon.jpg', 'dragon');
+  new Product('./img/pen.jpg', 'pen');
+  new Product('./img/pet-sweep.jpg', 'pet-sweep');
+  new Product('./img/scissors.jpg', 'scissors');
+  new Product('./img/shark.jpg', 'shark');
+  new Product('./img/sweep.png', 'sweep');
+  new Product('./img/tauntaun.jpg', 'tauntaun');
+  new Product('./img/unicorn.jpg', 'unicorn');
+  new Product('./img/usb.gif', 'usb');
+  new Product('./img/water-can.jpg', 'water-can');
+  new Product('./img/wine-glass.jpg', 'wine-glass');
 
   if (localStorage.getItem('status')) {
     var retentionDataArr = JSON.parse(localStorage.getItem('status'));
